8669bfc29dd2a663ae91d5cc9867cefd399eda0b
MADHANSHARON <madhanag2017@gmail.com>
2025-07-13 17:18:16 +0530

    Fix router: single BrowserRouter and clean App.jsx

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Projects from './Projects';
+import Main from '../main';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Hero />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
+  );
+}
+
+export default App;
