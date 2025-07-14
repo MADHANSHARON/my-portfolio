ccb869458acf64de7ee5b3bc2cfa2db3c8b653d7
MADHANSHARON <madhanag2017@gmail.com>
2025-07-14 16:36:28 +0530

    Initial commit

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -31,6 +31,7 @@ function Projects() {
           </div>
         </div>
 
+
       </div>
 
      
@@ -57,6 +58,27 @@ function Projects() {
       </div>
     </div>
   </div>
+  <div className="project-card">
+  <div className="project-card-content">
+    <h3>Currency Converter</h3>
+    <p>A simple and responsive currency converter built using React and a public exchange rate API.</p>
+    <div className="project-links">
+      <a href="https://madhansharon.github.io/Currency-converter/" target="_blank" rel="noreferrer">Live Demo</a>
+      <a href="https://github.com/MadhanSharon/Currency-converter" target="_blank" rel="noreferrer">View Code</a>
+    </div>
+  </div>
+</div>
+
+<div className="project-card">
+  <div className="project-card-content">
+    <h3>BMI Calculator</h3>
+    <p>A BMI calculator web app made with React to check body mass index based on user height and weight.</p>
+    <div className="project-links">
+      <a href="https://madhansharon.github.io/bmi-calculator/" target="_blank" rel="noreferrer">Live Demo</a>
+      <a href="https://github.com/MadhanSharon/bmi-calculator/" target="_blank" rel="noreferrer">View Code</a>
+    </div>
+  </div>
+</div>
 
       </div>
     </div>
